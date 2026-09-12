f926aa58bb10b2a8ed8507be2348e8e9ee471c95
TIMBENCOMPANY <timbencompany@gmail.com>
2026-09-12 14:26:27 +0300

    Force privacy redirect status

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
 import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/privacy-policy/", request.url), 301);
+  return NextResponse.redirect(new URL("/privacy-policy/", request.url), {
+    status: 301
+  });
 }
 
 export const config = {
